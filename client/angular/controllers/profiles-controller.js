4d833360ad856a78ebc6fa595ff91bf8cfc4f98f
Bryant <bryly27@sbcglobal.net>
2015-06-06 05:17:05 -0700

    fixed image

--- client/angular/controllers/profiles-controller.js
+++ client/angular/controllers/profiles-controller.js
@@ -7,6 +7,7 @@ cosplay.controller('profiles_controller', function($scope, profiles_factory) {
 
   profiles_factory.get_cosplay(function(results){
     $scope.profiles = results;
+    console.log(results);
   });
 
   profiles_factory.get_cosplayers(function(results){
